6c9b1648119add34123c97fb8ef1bd9eb65ee8cd
Felipe Bueno <felipebueno181@gmail.com>
2019-10-30 21:57:26 -0300

    finalização API - configuração .env

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default{
-    host: "smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: false,
     auth: {
-        user: "580f4db3556d47",
-        pass: "ee521a89418700"
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
     },
     default:{
         from: 'Equipe GoBarber <noreply@gobarber.com>'
